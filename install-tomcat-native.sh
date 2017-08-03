6859ba014435bf10ff226f541efed08cc11be2f7
Sergio Guillen <serguimant@gmail.com>
2017-08-03 01:12:16 -0400

    install tomcat-native lib

--- install-tomcat-native.sh
+++ install-tomcat-native.sh
@@ -6,3 +6,5 @@ cp /usr/local/tomcat/bin/tomcat-native.tar.gz /root
 tar xvzf tomcat-native.tar.gz
 cd tomcat-native-1.2.12-src/native
 ./configure --with-apr=/usr/local/apr --with-java-home=/usr/java/jdk1.8.0_144 --without-ssl
+make
+make install
