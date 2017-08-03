f47f4e0b2fc8df15c36e4d6d100db7da20b3e122
Sergio Guillen <serguimant@gmail.com>
2017-08-03 01:08:51 -0400

    fix apr build

--- install-tomcat-native.sh
+++ install-tomcat-native.sh
@@ -2,7 +2,7 @@
 
 cd /root
 
-cp $CATALINE_HOME/bin/tomcat-native.tar.gz /root
+cp $CATALINA_HOME/bin/tomcat-native.tar.gz /root
 tar xvzf tomcat-native.tar.gz
 cd tomcat-native-1.2.12-src/native
 ./configure --with-apr=/usr/local/apr --with-java-home=/usr/java/jdk1.8.0_144 --without-ssl
